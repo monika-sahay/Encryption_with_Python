a2ed9007487f85012a3ed5a7fde9575aaed5f0a8
monika-sahay <monika_sahay@yahoo.com>
2023-08-17 19:58:10 +0000

    added private key and prive_pem variables

--- gen_keys.py
+++ gen_keys.py
@@ -2,4 +2,14 @@ from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
 
 
+private_key = rsa.generate_private_key(
+    public_exponent = 65537,
+    key_size= 2048
+)
+priv_pem = private_key.private_bytes(
+    encoding = serialization.Encoding.PEM,
+    format= serialization.PrivateFormat.TraditionalOpenSSL,
+    encryption_algorithm=serialization.NoEncryption()
+)
 
+print(priv_pem)
